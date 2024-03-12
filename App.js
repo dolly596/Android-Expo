49f42e78134f7fbae4383b8e81d6e5f457cd31ef
dolly596 <83473173+dolly596@users.noreply.github.com>
2024-03-12 16:29:35 -0300

    Projeto Android

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Image style={styles.container}
+        source ={{uri:'https://imgs.search.brave.com/Q7iEMGO5US1m7xqhLwMkwXW4lKEyS_8cXar-FSWTzEw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9wMi50/cnJzZi5jb20vaW1h/Z2UvZmdldC9jZi83/NzQvMC9pbWFnZXMu/dGVycmEuY29tLzIw/MTkvMDQvMDkvbWMt/em9pLWRlLWdhdG8u/anBlZw'}}
+      
+      />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    height:300,
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
